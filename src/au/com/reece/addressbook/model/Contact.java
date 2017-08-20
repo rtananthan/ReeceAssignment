d6dc29c4a64e167aafde28d691a5a7541dc785e5
Ananthan Rudran <Ananthan@Ananthans-MacBook-Pro.local>
2017-08-20 21:07:32 +1000

    Initial commit

---
+++ src/au/com/reece/addressbook/model/Contact.java
@@ -0,0 +1,18 @@
+package au.com.reece.addressbook.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Ananthan on 20/8/17.
+ */
+public class Contact implements Serializable {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNo;
+
+
+
+}
